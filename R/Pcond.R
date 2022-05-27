2b3693eb5287fec84c42ed8bddcfe3ad8e940906
sophia-lambert <103935357+sophia-lambert@users.noreply.github.com>
2022-05-27 14:10:58 +0200

    Correcting orthograph.

--- R/Pcond.R
+++ R/Pcond.R
@@ -1,16 +1,16 @@
-#' Calculating the conditionning of the birth-death process on the age of the process and its survival.
+#' Calculating the conditioning of the birth-death process on the age of the process and its survival.
 #'
 #' @param t Numeric. The time of the start of the birth-death process in millions of years.
 #' @param r Numeric. The net diversification rate : \eqn{\lambda - \mu}. The units are in events/lineages/millions years.
 #' @param epsi Numeric. The turnover rate : \eqn{\mu / \lambda}. The units are in events/lineages/millions years.
-#' @param y Numeric. The sampling probability also called sampling fraction : \eqn{\k / \N}
+#' @param y Numeric. The sampling probability also called sampling fraction : \eqn{k / N}
 #'
-#' @return The probabillity of the birth-death process starting with one lineage at time t and surviving at present.
+#' @return The probability of the birth-death process starting with one lineage at time t and surviving at present.
 #'
 #' @author Sophia Lambert
 #'
 
-#\code{\link{likelihood_bdRho}}, \code{\link{fitMCMC_bdRho}}
+#@seealso \code{\link{likelihood_bdRho}}, \code{\link{fitMCMC_bdRho}}
 
 
 Pcond <- function(t, r, epsi, y){
