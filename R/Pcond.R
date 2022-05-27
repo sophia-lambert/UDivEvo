974e29165e00c12a75257ecaa4fc8c8ce5828a0f
sophia-lambert <103935357+sophia-lambert@users.noreply.github.com>
2022-05-27 13:25:36 +0200

    Creating thhe function Pcond.

---
+++ R/Pcond.R
@@ -0,0 +1,19 @@
+#' Calculating the conditionning of the birth-death process on the age of the process and its survival.
+#'
+#' @param t Numeric. The time of the start of the birth-death process in millions of years.
+#' @param r Numeric. The net diversification rate : \eqn{\lambda - \mu}. The units are in events/lineages/millions years.
+#' @param epsi Numeric. The turnover rate : \eqn{\mu / \lambda}. The units are in events/lineages/millions years.
+#' @param y Numeric. The sampling probability also called sampling fraction : \eqn{\k / \N}
+#'
+#' @return The probabillity of the birth-death process starting with one lineage at time t and surviving at present.
+#'
+#' @author Sophia Lambert
+#'
+##' @seealso \code{\link{function}}
+
+
+Pcond <- function(t, r, epsi, y){
+  1/
+    (1+
+       y/(1-epsi)*(exp(r*t)-1))
+}
