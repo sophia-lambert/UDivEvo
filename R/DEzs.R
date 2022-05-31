13f7af88dcf2a45bc516c47963dba4f2b8da539e
sophia-lambert <103935357+sophia-lambert@users.noreply.github.com>
2022-06-01 00:17:41 +0200

    Correcting importing functions.

--- R/DEzs.R
+++ R/DEzs.R
@@ -189,7 +189,7 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
     if(blocks){
       ### Update the groups.
       if(iter == groupStart+ Bcount*groupIntervall){
-        blockSettings <- BayesianTools::updateGroups(chain = pChain[1:counter,, ], blockUpdate)
+        blockSettings <- updateGroups(chain = pChain[1:counter,, ], blockUpdate)
         BlockStart <- TRUE
         Bcount <- Bcount + 1
       }
@@ -204,12 +204,12 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
       for(i in 1:Npop){
         # select to random different individuals (and different from i) in rr, a 2-vector
         rr <- sample.int(M, 3, replace = FALSE)
-        if(runif(1) < pSnooker) {
+        if(stats::runif(1) < pSnooker) {
           z <- Z[rr[3],]
           x_z <- X[i,] - z
           D2 <- max(sum(x_z*x_z), 1.0e-300)
           projdiff <- sum((Z[rr[1],] -Z[rr[2],]) * x_z)/D2 # inner_product of difference with x_z / squared norm x_z
-          gamma_snooker <- runif(1, min=1.2,max=2.2)
+          gamma_snooker <- stats::runif(1, min=1.2,max=2.2)
 
           x_prop[i,] <-  X[i,] + gamma_snooker * projdiff * x_z
           x_z <- x_prop[i,] - z
@@ -217,16 +217,16 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
           r_extra[i] <- Npar12 * (log(D2prop) - log(D2))
 
         } else {
-          if ( runif(1)< pGamma1 ) { gamma_par = F1 # to be able to jump between modes
+          if ( stats::runif(1)< pGamma1 ) { gamma_par = F1 # to be able to jump between modes
           } else {
-            gamma_par = F2 * runif(Npar, min=1-eps.mult, max=1+eps.mult)    # multiplicative error to be applied to the difference
+            gamma_par = F2 * stats::runif(Npar, min=1-eps.mult, max=1+eps.mult)    # multiplicative error to be applied to the difference
             # gamma_par = F2
           }
           rr = sample.int(M, 2, replace = FALSE)
           if (eps.add ==0) {  # avoid generating normal random variates if possible
             x_prop[i,] = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],])
           } else {
-            x_prop[i,] = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],])  +  eps.add*rnorm(Npar,0,1)
+            x_prop[i,] = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],])  +  eps.add*stats::rnorm(Npar,0,1)
           }
           r_extra = rep(0, Npop)
         }
@@ -235,7 +235,7 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
 
       if(BlockStart){
         # Get the current group and update the proposal accordingly
-        Member <- BayesianTools::getBlock(blockSettings)
+        Member <- getBlock(blockSettings)
         x_prop[,-Member] <- X[,-Member]
         ####
       }
@@ -247,7 +247,7 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
       # evaluate acceptance
       for(i in 1:Npop){
         if(!is.na(logfitness_x_prop[i,1] - logfitness_X[i,1])){
-          if ((logfitness_x_prop[i,1] - logfitness_X[i,1] + r_extra[i]) > log(runif(1))){
+          if ((logfitness_x_prop[i,1] - logfitness_X[i,1] + r_extra[i]) > log(stats::runif(1))){
             # accept <- accept + 1
             X[i,] <- x_prop[i,]
             logfitness_X[i,] <- logfitness_x_prop[i,]
@@ -261,34 +261,34 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
       for (i in 1:Npop){
         # select to random different individuals (and different from i) in rr, a 2-vector
         rr <- sample.int(M, 3, replace = FALSE)
-        if(runif(1) < pSnooker) {
+        if(stats::runif(1) < pSnooker) {
           z <- Z[rr[3],]
           x_z <- X[i,] - z
           D2 <- max(sum(x_z*x_z), 1.0e-300)
           projdiff <- sum((Z[rr[1],] -Z[rr[2],]) * x_z)/D2 # inner_product of difference with x_z / squared norm x_z
-          gamma_snooker <- runif(1, min=1.2,max=2.2)
+          gamma_snooker <- stats::runif(1, min=1.2,max=2.2)
           x_prop <- X[i,] + gamma_snooker * projdiff * x_z
           x_z <- x_prop - z
           D2prop <- max(sum(x_z*x_z), 1.0e-300)
           r_extra <- Npar12 * (log(D2prop) - log(D2))
         } else {
 
-          if ( runif(1)< pGamma1 ) { gamma_par = F1 # to be able to jump between modes
+          if ( stats::runif(1)< pGamma1 ) { gamma_par = F1 # to be able to jump between modes
           } else {
-            gamma_par = F2 * runif(Npar, min=1-eps.mult, max=1+eps.mult)    # multiplicative error to be applied to the difference
+            gamma_par = F2 * stats::runif(Npar, min=1-eps.mult, max=1+eps.mult)    # multiplicative error to be applied to the difference
             # gamma_par = F2
           }
           rr = sample.int(M, 2, replace = FALSE)
           if (eps.add ==0) {  # avoid generating normal random variates if possible
             x_prop = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],]) } else {
-              x_prop = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],])  +  eps.add*rnorm(Npar,0,1)
+              x_prop = X[i,] + gamma_par * (Z[rr[1],]-Z[rr[2],])  +  eps.add*stats::rnorm(Npar,0,1)
             }
           r_extra = 0
 
         }
         if(BlockStart){
           # Get the current group and update the proposal accordingly
-          Member <- BayesianTools::getBlock(blockSettings)
+          Member <- getBlock(blockSettings)
           x_prop[-Member] <- X[i,-Member]
           ####
         }
@@ -299,7 +299,7 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
 
         # evaluate acceptance
         if(!is.na(logfitness_x_prop[1] - logfitness_X[i,1])){
-          if ((logfitness_x_prop[1] - logfitness_X[i,1] + r_extra) > log(runif(1))){
+          if ((logfitness_x_prop[1] - logfitness_X[i,1] + r_extra) > log(stats::runif(1))){
             # accept <- accept + 1
             X[i,] <- x_prop
             logfitness_X[i,] <- logfitness_x_prop
@@ -383,4 +383,92 @@ DEzs <- function(bayesianSetup, save_inter = NULL, index_saving = NULL,
 
   list(Draws = pChain,  X = as.matrix(X[,1:Npar]), Z = Z)
 
+}
+
+
+
+
+# getBlock function not exported in BayesianTools
+
+getBlock <- function(blockSettings){
+  groups <- blockSettings$cT
+  pGroup <- blockSettings$pGroup
+  pSel <- blockSettings$pSel
+
+
+  nGroups = max(groups)
+  if(nGroups == 1) return(1:length(groups))
+  if (is.null(pGroup)) pGroup = rep(1,nGroups)
+  if(length(pSel) > nGroups) pSel <- pSel[1:nGroups]
+  pSel = c(pSel, rep(0,nGroups - length(pSel)))
+  groupsToSample = sample.int(nGroups, 1, prob = pSel)
+
+  selectedGroups = sample.int(nGroups,groupsToSample, prob = pGroup[1:nGroups])
+  GroupMember  <- which(is.element(groups,selectedGroups))
+  return(GroupMember)
+
+}
+
+
+
+
+# updateGroups function not exported in BayesianTools
+
+updateGroups <- function(chain,blockSettings){
+
+  settings <- getBlockSettings(blockSettings)
+  blockUpdateType  <- settings$blockUpdateType
+
+  switch(blockUpdateType,
+         "correlation" = {
+           ## (Pair wise) Correlation in the parameters
+           cormat <- abs(stats::cor(chain[,1:(ncol(chain)-3),sample(1:dim(chain)[3],1)]))
+           diag(cormat) <- 0
+           # Correct for NA and Inf values as this could cause error in as.dist()
+           cormat[c(which(is.na(cormat)),which(cormat == Inf),which(cormat == -Inf)) ] <- 0
+           tree <- stats::hclust(stats::as.dist(1-cormat))  # get tree based on distance(dissimilarity = 1-cor).
+           cT <- stats::cutree(tree, k = settings$k, h = settings$h) # get groups. With h we can manipulate the strength of the interaction.
+         },
+         "user" = {
+           cT <-  settings$groups
+         },
+         "random" = {
+           pool <- c(1:settings$k, sample(1:settings$k, (ncol(chain)-3-settings$k)))
+           cT <- sample(pool)
+         }
+  )
+
+  pSel <- settings$pSel
+  if(is.null(pSel) && is.null(settings$pGroup)) pSel = rep(1,ncol(chain)-3)
+  return(list(cT = cT, pGroup = settings$pGroup, pSel = pSel))
+}
+
+
+
+
+# getBlockSettings function not exported in BayesianTools
+
+getBlockSettings <- function(blockUpdate){
+
+  h <- k <- pSel <- pGroup <- groups <- NULL
+  blockUpdateType <- blockUpdate[[1]]
+
+  switch(blockUpdateType,
+         "correlation" = {
+           h <- blockUpdate$h
+           k <- blockUpdate$k
+           pSel <- blockUpdate$pSel
+           pGroup <- blockUpdate$pGroup
+         },
+         "random"={
+           k <- blockUpdate$k
+         },
+         "user"= {
+           groups <- blockUpdate$groups
+           pSel <- blockUpdate$pSel
+           pGroup <- blockUpdate$pGroup
+         })
+
+  return(list(blockUpdateType = blockUpdateType, h = h, k = k, pSel = pSel,
+              pGroup = pGroup, groups = groups))
 }
