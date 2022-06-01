0d69f53f9764c4295407002d883653a0f3302a7f
sophia-lambert <103935357+sophia-lambert@users.noreply.github.com>
2022-06-01 14:35:55 +0200

    Erased because make Roxygen angry.

--- R/MemoForDevelopment.R
+++
@@ -1,9 +0,0 @@
-# to run sometimes
-
-# to tidy the description
-usethis::use_tidy_description()
-
-# to rebuild the documentation
-devtools::document()
-
-# minor performance penalty using :: on the order of 5µs, so it will only matter if you call the function millions of times
